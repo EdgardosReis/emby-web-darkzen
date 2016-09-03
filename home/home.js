4026a3068afda793145635b8f93516ec64493f3b
EdgardosReis <edgardosreis@hotmail.com>
2016-09-03 01:57:37 +0100

    movies view main item

--- home/home.js
+++ home/home.js
@@ -185,7 +185,6 @@ define(['connectionManager', 'loading', './../components/tabbedpage', 'backdrop'
                         break;
                     case 'movies':
                         viewName = 'movies';
-                        debugger;
                         page.querySelector('button[data-type="movies"]').addEventListener('click', function () {
                             Emby.Page.show(Emby.PluginManager.mapRoute(skinInfo.id, 'movies/movies.html?parentid=' + id));
                         });                                           
